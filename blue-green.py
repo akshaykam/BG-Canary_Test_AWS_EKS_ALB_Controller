58f77758f4f53d2036fefad4d355240de2ef1a35
akshaykam <akshaykam@cybage.com>
2026-01-30 16:10:19 +0530

    added ingress, config, updated deployment, image tags

--- blue-green.py
+++ blue-green.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
    return render_template(environ['html_name'])
+
+@app.route('/health')
+def health():
+   return {"status": "ok"}, 200
 if __name__ == '__main__':
    app.run()
